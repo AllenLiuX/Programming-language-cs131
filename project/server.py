aa4342cea021b4e7e7a28aa58ef167a44c1d5a92
VincentLiu <13120200491@163.com>
2021-03-11 20:17:07 +0800

    modify done

--- project/server.py
+++ project/server.py
@@ -4,7 +4,7 @@ import asyncio
 import aiohttp
 import json
 import os
-import logging
+# import logging
 
 APIKEY = 'AIzaSyAI4IRkMeECJEBQCKH6wrRiWu1X4k76xII'
 
@@ -31,30 +31,47 @@ async def fetch(url):
             ssl=False,
         ),
     ) as session:
-        async with session.get(url) as resp:
-            response = await resp.json()
+        async with session.get(url) as response:
+            response = await response.json()
             # print(response)
             return response
 
 
-async def writeResponse(writer, msg):
-    if not msg:
-        return
-    try:
-        writer.write(msg.encode())
-        print("writing response:\n" + msg)
-        await writer.drain()
-        writer.write_eof()
-    except:
-        print('error writing msg: %s' % msg)
+async def retrieve_nearby(coord, radius, place_num):
+    # async with aiohttp.ClientSession() as session:
+    coord_li = coord_trans(coord)
+    coord_in = coord_li[0]+','+coord_li[1]
+    url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={coord_in}&radius={radius}&key={APIKEY}'
+    write_log(f'Getting info at: {url}')
+    # async with session.get(url) as response:
+    #     places = await response.json(loads=json.loads)
+    places = await fetch(url)
+    write_log(f'Retrieved {len(places["results"])} places')
+    if len(places['results']) > int(place_num):
+        places['results'] = places['results'][:int(place_num)]
+    return str(json.dumps(places, indent=4))
+# def coord_trans(coord):
+#     split = max(coord.rfind('+'), coord.rfind('-'))
+#     return f'{coord[:split]},{coord[split:]}'
 
 
-def parse_coord(coord):
-    split = max(coord.rfind('+'), coord.rfind('-'))
-    return f'{coord[:split]},{coord[split:]}'
+def coord_trans(coord):
+    ind = 0
+    count = 0
+    lat = ''
+    lon = ''
+    for c in coord:
+        if c == '+' or c == '-':
+            count = count + 1
+            if count == 2:
+                lat = coord[:(ind - 1)]
+                lon = coord[ind:]
+        ind = ind + 1
+    # return f'{lat},{lon}'
+    return [lat, lon]   # type str
 
 
-def is_numeric(string):
+def is_num(string):
     try:
         float(string)
         return True
@@ -62,156 +79,151 @@ def is_numeric(string):
         return False
 
 
-def print_and_log(message):
+def write_log(message):
     print(message)
-    logging.info(message)
+    logFile.write(message)
+    # logging.info(message)
 
 
-def print_and_error(message):
+def write_error(message):
+    message = 'ERROR! ' + message
     print(message)
-    logging.error(message)
+    logFile.write(message)
+    # logging.error(message)
 
 
 class Server:
     def __init__(self, server_name):
         self.server_name = server_name
         self.port = PORT_NUMS[server_name]
-        self.client_dict = {}
+        self.clients = {}
 
-        # create timestamped server log file
-        if not os.path.exists('logs'):
-            os.makedirs('logs')
+        # # create timestamped server log file
+        # if not os.path.exists('logs'):
+        #     os.makedirs('logs')
 
-        now = datetime.datetime.now()
-        now_string = now.strftime('%Y-%m-%dT%H:%M:%S')
-        logging.basicConfig(filename=f'logs/{server_name}-{now_string}.txt',
-                            format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG)
-        logging.info(f'Opened server {server_name} session')
-
+        # now = datetime.datetime.now()
+        # now_string = now.strftime('%Y-%m-%dT%H:%M:%S')
+        # logging.basicConfig(filename=f'logs/{server_name}-{now_string}.txt',
+        #                     format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG)
+        write_log(f'Opened server {server_name}')
+        # logging.info(f'Opened server {server_name} session')
 
     async def handle_cmd(self, reader, writer):
         while not reader.at_eof():
             data = await reader.readline()
             message = data.decode()
             addr = writer.get_extra_info('peername')
-            print_and_log(f'Received from {addr}: {message}')
+            write_log(f'Received from server {addr}: {message}')
 
-            resp = await self.parse_message(message)
-            if resp:
-                writer.write(resp.encode())
-                print_and_log(f"Responded to client {addr}: {resp}")
-
+            response = await self.parse_cmd(message)
+            if response:
+                # await write_response(writer, response)
+                writer.write(response.encode())
+                write_log(f"Responded to client {addr}: {response}")
             writer.close()
-            print_and_log(f'Closed the connection with {addr}')
+            write_log(f'Closed connection with {addr}')
 
-    async def parse_message(self, message):
-        command_table = {
-            'IAMAT': self.handle_i_am_at,
-            'WHATSAT': self.handle_whats_at,
-            'AT': self.handle_at
+    async def parse_cmd(self, message):
+        cmd_funcs = {
+            'IAMAT': self.i_am_at,
+            'WHATSAT': self.whats_at,
+            'AT': self.at
         }
-        num_args = {
+        arg_nums = {
             'IAMAT': 4,
             'WHATSAT': 4,
             'AT': 6
         }
+
+        # check empty cmd, wrong cmd, and wrong number of args
         message_list = [msg for msg in message.strip().split() if msg]
-        if len(message_list) == 0:
-            print_and_error(f'Invalid message: {message}')
+        if len(message_list) == 0 or message_list[0] not in cmd_funcs:
+            write_error(f'Invalid cmd: {message}')
             return f'? {message}'
-        if not message_list[0] in command_table:
-            print_and_error(f'Invalid message: {message}')
-            return f'? {message}'
-        if len(message_list) != num_args[message_list[0]]:
-            print_and_error(f'Invalid number of args for {message_list[0]}: {len(message_list)}')
+        elif len(message_list) != arg_nums[message_list[0]]:
+            write_error(f'Invalid arg num for {message_list[0]}: {len(message_list)}')
             return f'? {message}'
 
-        cmd = command_table.get(message_list[0])
+        func = cmd_funcs.get(message_list[0])
         try:
-            response = await cmd(*message_list[1:])
+            response = await func(*message_list[1:])
             return response
         except Exception as e:
-            print_and_error(f'Error while handling command: {e}')
+            write_error(f'failed at command: {e}')
             return f'? {message}'
 
-    async def handle_i_am_at(self, client, coord, timestamp):
-        coords = [s for s in coord.replace('-', '+').split('+') if s != ""]
-        if len(coords) != 2 or not is_numeric(coords[0]) or not is_numeric(coords[1]):
-            raise Exception(f'invalid IAMAT message coord field: {coord}')
-        if not is_numeric(timestamp):
-            raise Exception(f'invalid IAMAT message timestamp field: {timestamp}')
+    async def i_am_at(self, client, coord, timestamp):
+        # coords = [s for s in coord.replace('-', '+').split('+') if s != ""]
+        coords = coord_trans(coord)
+        if len(coords) != 2 or not is_num(coords[0]) or not is_num(coords[1]):
+            raise Exception(f'Invalid IAMAT coord: {coord}')
+        if not is_num(timestamp):
+            raise Exception(f'Invalid IAMAT timestamp: {timestamp}')
 
-        time_diff = datetime.datetime.now().timestamp() - float(timestamp)
-        time_str = '+' + str(time_diff) if time_diff > 0 else str(time_diff)
-        response_str = f'AT {self.server_name} {time_str} {client} {coord} {timestamp}'
-
-        self.client_dict[client] = {'timestamp': timestamp, 'msg': response_str}
-        await self.propagate(response_str)
-        return response_str
-
-    async def handle_whats_at(self, client, radius, numplaces):
-        if not client in self.client_dict:
-            raise Exception(f'invalid WHATSAT message client field: {client}')
-        if not is_numeric(radius) or float(radius) < 0 or float(radius) > 50:
-            raise Exception(f'invalid WHATSAT message radius field: {radius}')
-        if not is_numeric(numplaces) or float(numplaces) < 0 or float(numplaces) > 20:
-            raise Exception(f'invalid WHATSAT message numplaces field: {numplaces}')
-
-        at_str = self.client_dict[client]['msg']
-        coord = at_str.split()[4]
-        places_str = (await self.find_places(coord, radius, numplaces)).rstrip('\n')
-        return f'{at_str}\n{places_str}\n\n'
-
-    async def handle_at(self, server, time_diff, client, coord, timestamp):
-        # no need to error check because only servers will send AT's
-        if not client in self.client_dict or timestamp > self.client_dict[client]['timestamp']:
-            at_str = f'AT {server} {time_diff} {client} {coord} {timestamp}'
-            self.client_dict[client] = {'timestamp': timestamp, 'msg': at_str}
-            print_and_log(f'Received new propagated information about location of {client}')
-            await self.propagate(at_str)
+        time_delta = datetime.datetime.now().timestamp() - float(timestamp)
+        if time_delta > 0:
+            time_delta = '+' + str(time_delta)
         else:
-            print_and_log(f'Received redundant propagated info for client {client}, ignoring.')
+            time_delta = str(time_delta)
+        # time_delta = '+' + str(time_delta) if time_delta > 0 else str(time_delta)
+        response = f'AT {self.server_name} {time_delta} {client} {coord} {timestamp}'
 
-    async def find_places(self, coord, radius, numplaces):
-        async with aiohttp.ClientSession() as session:
-            url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={self.parse_coord(coord)}&radius={radius}&key={APIKEY}'
-            print_and_log(f'Attempting to GET from {url}')
-            # async with session.get(url) as response:
-            #     places = await response.json(loads=json.loads)
-            places = await fetch(url)
-            print_and_log(f'Retrieved {len(places["results"])} places from Google Places API.')
-            if len(places['results']) > int(numplaces):
-                places['results'] = places['results'][:int(numplaces)]
+        self.clients[client] = {'timestamp': timestamp, 'message': response}
+        await self.propagate(response)
+        return response
 
-            return str(json.dumps(places, indent=4))
+    async def whats_at(self, client, radius, place_num):
+        if not client in self.clients:
+            raise Exception(f'Invalid WHATSAT client: {client}')
+        if not is_num(radius) or float(radius) < 0 or float(radius) > 50:
+            raise Exception(f'Invalid WHATSAT radius: {radius}')
+        if not is_num(place_num) or float(place_num) < 0 or float(place_num) > 20:
+            raise Exception(f'Invalid WHATSAT places num: {place_num}')
+
+        at_cmd = self.clients[client]['message']
+        coord = at_cmd.split()[4]
+        places_str = (await retrieve_nearby(coord, radius, place_num)).rstrip('\n')
+        return f'{at_cmd}\n{places_str}\n\n'
+
+    async def at(self, server, time_delta, client, coord, timestamp):
+        # no need to error check because only servers will send AT's
+        if client not in self.clients or timestamp > self.clients[client]['timestamp']:
+            at_cmd = f'AT {server} {time_delta} {client} {coord} {timestamp}'
+            self.clients[client] = {'timestamp': timestamp, 'message': at_cmd}
+            write_log(f'Received propagation for {client}')
+            await self.propagate(at_cmd)
+        else:
+            write_log(f'Redundant propagation for client {client}. Skipped.')
+
 
     async def propagate(self, message):
-        for neighbor in CONNECTIONS[self.server_name]:
+        for reachable_server in CONNECTIONS[self.server_name]:
             try:
-                _, writer = await asyncio.open_connection('127.0.0.1', PORT_NUMS[neighbor])
+                reader, writer = await asyncio.open_connection('127.0.0.1', PORT_NUMS[reachable_server])
 
+                # await write_response(writer, message)
                 writer.write(message.encode())
-                print_and_log(f'Propagating message to {neighbor}: {message}')
+                write_log(f'Propagating to {reachable_server}: {message}')
 
                 writer.close()
                 await writer.wait_closed()
-                print_and_log(f'Closing connection with {neighbor}')
-
+                write_log(f'Closing connection with {reachable_server}')
             except:
-                print_and_log(f'Error connecting to server {neighbor}')
+                write_log(f'Failed to connect to server {reachable_server}')
 
-    async def serve(self):
+    async def server_entry(self):
         server = await asyncio.start_server(self.handle_cmd, '127.0.0.1', self.port)
         addr = server.sockets[0].getsockname()
-        print_and_log(f'{self.server_name} serving on {addr}')
+        write_log(f'{self.server_name} serving on {addr}')
         async with server:
             await server.serve_forever()
 
     def run(self):
         try:
-            asyncio.run(self.serve())
+            asyncio.run(self.server_entry())
         except KeyboardInterrupt:
-            print_and_log(f'Server {self.server_name} closed.')
+            write_log(f'Server {self.server_name} closed.')
 
 
 if __name__ == "__main__":
@@ -226,5 +238,12 @@ if __name__ == "__main__":
         print(f"error: invalid server name {server_name}")
         exit()
 
+    log = server_name + "-log.txt"
+    global logFile
+    open(log, "w").close()  # clears the file
+    logFile = open(log, 'w')
+
     s = Server(server_name)
     s.run()
+    logFile.close()
+
